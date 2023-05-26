485aa6d496b21bce13c075b36b8969f9b7cd317d
HanDaWoon <bookpia.han@gmail.com>
2023-05-26 14:16:40 +0900

    Feat : cors 적용

--- src/main/kotlin/com/boseyo/backend/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/boseyo/backend/config/WebSecurityConfiguration.kt
@@ -2,7 +2,6 @@ package com.boseyo.backend.config
 
 import com.boseyo.backend.jwt.JwtSecurityConfig
 import com.boseyo.backend.jwt.JwtTokenProvider
-import org.apache.catalina.filters.CorsFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.access.AccessDeniedHandler
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
