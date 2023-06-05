7fd6c64787bfc05a239dd31829d8c54ecac3bdd2
HanDaWoon <bookpia.han@gmail.com>
2023-06-05 20:07:49 +0900

    Fix : 에러 해결?
    RequestBody 추가

--- src/main/kotlin/com/boseyo/backend/controller/BoardController.kt
+++ src/main/kotlin/com/boseyo/backend/controller/BoardController.kt
@@ -1,26 +1,17 @@
 package com.boseyo.backend.controller
 
 import com.boseyo.backend.dto.BoardDto
-import com.boseyo.backend.entity.BoardEntity
 import com.boseyo.backend.service.BoardService
 import jakarta.persistence.*
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/board")
 class BoardController (@Autowired private val boardService: BoardService){
     //게시글 작성
     @PostMapping("/write")
-    fun writePost(boardDto: BoardDto):Long?{
+    fun writePost(@RequestBody boardDto: BoardDto):Long?{
         return boardService.savePost(boardDto)
     }
 
